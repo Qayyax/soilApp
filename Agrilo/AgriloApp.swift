e334c1b64bf28499b5b47d204683333036f007c5
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-08-10 16:18:36 -0400

    Initial Commit

---
+++ Agrilo/AgriloApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgriloApp.swift
+//  Agrilo
+//
+//  Created by Qayyax on 8/10/25.
+//
+
+import SwiftUI
+
+@main
+struct AgriloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AgriloTests/AgriloTests.swift
@@ -0,0 +1,17 @@
+//
+//  AgriloTests.swift
+//  AgriloTests
+//
+//  Created by Qayyax on 8/10/25.
+//
+
+import Testing
+@testable import Agrilo
+
+struct AgriloTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
