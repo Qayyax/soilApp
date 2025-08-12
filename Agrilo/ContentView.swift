85d0cdf15ef49088111850ff60234c614ef6cfba
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-08-11 20:02:00 -0400

    still needs fix

--- Agrilo/ContentView.swift
+++ Agrilo/ContentView.swift
@@ -9,28 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
   var body: some View {
-      TabView{
-        // home page
-        Tab("", systemImage: "house") {
-          HomeView()
-        }
-        
-        Tab("", systemImage: "thermometer.sun") {
-          TemperatureView()
-        }
-        
-        Tab("", systemImage: "barcode.viewfinder") {
-          ScannerView()
-        }
-        
-        Tab("", systemImage: "books.vertical") {
-          LibraryView()
-        }
-
-        Tab("", systemImage: "gearshape") {
-          SettingsView()
-        }
-    }
+    MainTabView()
   }
 }
   
@@ -44,21 +23,6 @@ enum TabbedItems: Int, CaseIterable{
   case scanner
   case library
   case settings
-    
-    var title: String{
-        switch self {
-        case .home:
-            return "Home"
-        case .temperature:
-            return "Temperature"
-        case .scanner:
-            return "Scanner"
-        case .library:
-            return "Library"
-        case .settings:
-          return "Settings"
-        }
-    }
     
     var iconName: String{
         switch self {
@@ -74,4 +38,58 @@ enum TabbedItems: Int, CaseIterable{
           return "gearshape"
         }
     }
+}
+
+struct MainTabView: View {
+  @State var selectedTab = 0
+  var body: some View {
+    
+    ZStack(alignment: .bottom) {
+      TabView(selection: $selectedTab) {
+        HomeView()
+          .tag(0)
+        TemperatureView()
+          .tag(1)
+        ScannerView()
+          .tag(2)
+        LibraryView()
+          .tag(3)
+        SettingsView()
+          .tag(4)
+      }
+      .ignoresSafeArea()
+      
+      VStack{
+        HStack{
+          ForEach((TabbedItems.allCases), id: \.self){ item in
+            Button{
+              selectedTab = item.rawValue
+            } label: {
+              CustomTabItem(imageName: item.iconName , isActive: (selectedTab == item.rawValue))
+            }
+          }
+        }
+        .padding(5)
+      }
+      .frame(height: 60)
+      .background(.gray)
+      .cornerRadius(30)
+      .padding(.horizontal, 20)
+    }
+  }
+}
+
+extension MainTabView {
+  func CustomTabItem(imageName: String, isActive: Bool) -> some View {
+    HStack {
+      Image(systemName: imageName)
+        .resizable()
+        .renderingMode(.template)
+        .foregroundColor(isActive ? .yellow : .white)
+        .frame(width: 20, height: 20)
+    }
+    .frame(width: 60, height: 60)
+    .background(.black)
+    .cornerRadius(30)
+  }
 }
